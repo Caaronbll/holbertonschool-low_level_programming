420eecbef46aa1fc5eb28c1d48f734fedfda0a68
Caaronbll <aaronbll46@gmail.com>
2023-01-30 14:18:17 -0800

    q4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+nclude "main.h"
+
+/**
+ * _islower - checks for lowercase
+ * @c: c is an ascii character
+ *
+ * Return: 1 if lowercase
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'A' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+       	{
+		return (0);
+	}
+}
