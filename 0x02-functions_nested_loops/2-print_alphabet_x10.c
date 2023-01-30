588cd5134b5e7dca410383d6cfed3f5f9112da0d
Caaronbll <aaronbll46@gmail.com>
2023-01-30 12:47:37 -0800

    q0

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		char ch = 'a';
+
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		i++;
+	}
+	_putchar('\n');
+}
