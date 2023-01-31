23685c31a132a7f3208e15eb397c6419237fb246
Caaronbll <aaronbll46@gmail.com>
2023-01-30 19:50:23 -0800

    q11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,30 +14,19 @@ void print_to_98(int n)
 {
 	if (n < 98)
 	{
-		while (n <= 98)
+		while (n < 98)
 		{
-			printf("%d", n);
-			if (n != 98)
-			{
-				printf(", ");
-			}
-			n++;
+		printf("%d, ", n);
+		n++;
 		}
 	}
 	else if (n > 98)
 	{
-		while (n >= 98)
+		while (n > 98)
 		{
-			printf("%d", n);
-			if (n != 98)
-			{
-				printf(", ");
-			}
-			n--;
+		printf("%d, ", n);
+		n--;
 		}
 	}
-	else
-	{
-		printf("98\n");
-	}
+	printf("98\n");
 }
