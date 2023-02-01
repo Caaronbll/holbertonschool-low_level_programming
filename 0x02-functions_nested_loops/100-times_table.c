a2395505cbaad4dffcb6b8b6728afd35a7d53bb9
Caaronbll <aaronbll46@gmail.com>
2023-01-31 16:14:02 -0800

    a

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print times table
+ *
+ * Return: Always 0.
+ */
+
+void print_times_table(int n)
+{
+	int x, y;
+	for (x = 0;x <= n; x++)
+	{
+		for (y = 0;y <= n; y++)
+		{
+		printf(x*y);
+		printf(', ');
+		}
+	}
+	printf('\n');
+}
