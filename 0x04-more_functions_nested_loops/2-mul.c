051a5b83b9338894f13a487e707701c616a106c7
Caaronbll <aaronbll46@gmail.com>
2023-02-02 14:31:46 -0800

    q2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiply a and b
+ * @a - miti
+ * @b - milti
+ * Return: product
+ */
+
+int mul(int a, int b)
+{
+	int p;
+
+	p = (a*b);
+	return (p);
+}
