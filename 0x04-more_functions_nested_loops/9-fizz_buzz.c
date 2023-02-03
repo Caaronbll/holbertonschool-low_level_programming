13476669d8d1e69dc1544feb8355f60007274c66
Caaronbll <aaronbll46@gmail.com>
2023-02-03 15:05:29 -0800

    q9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,23 +12,23 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-	if (i % 15 == 0)
-	{
-		printf("FizzBuzz ");
+		if (i % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
 	}
-	else if (i % 3 == 0)
-	{
-		printf("Fizz ");
-	}
-	else if (i % 5 == 0)
-	{
-		printf("Buzz ");
-	}
-	else
-	{
-		printf("%d ", i);
-	}
-}
 printf("\n");
 return (0);
 }
