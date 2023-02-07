7a8f270fc0316294651b38102a55ad77112e5f02
Caaronbll <aaronbll46@gmail.com>
2023-02-07 08:13:38 -0800

    q9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,7 +9,7 @@ int main(void)
 {
 int i;
 
-	for (i = 1; i <= 100; i++)
+	for (i = 1; i <= 99; i++)
 	{
 		if (i % 15 == 0)
 		{
@@ -28,6 +28,7 @@ int i;
 			printf("%d ", i);
 		}
 	}
+printf("Buzz");
 printf("\n");
 return (0);
 }
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,5 @@
+#include "main.h"
+
+/**
+ * puts2 - prints everyother char in a string
+ *
