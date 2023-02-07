4e0fa366a847636da8b5ba15edfdde009d541055
Caaronbll <aaronbll46@gmail.com>
2023-02-06 17:23:27 -0800

    q5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * @s: string
+ * Return: void.
+ */
+
+void rev_string(char *s)
+{
+	int i, m, h;
+	char f, l;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	m = i - 1;
+	h = m / 2;
+	while (h >= 0)
+	{
+		f =  s[m - h];
+		l = s[h];
+		s[h] = f;
+		s[m - h] = l;
+		h--;
+	}
+}		
