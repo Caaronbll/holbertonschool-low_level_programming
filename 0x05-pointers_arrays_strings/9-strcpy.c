00280953541b6dc72e081b2dd5c521ad7c8b487a
Caaronbll <aaronbll46@gmail.com>
2023-02-09 08:38:10 -0800

    q9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies string
+ * @dest: destination 
+ * @src: source
+ * Return: copy of string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+	
+	while (*(src + i) != '\0')
+	{
+		dest[i] = *(src + i);
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
