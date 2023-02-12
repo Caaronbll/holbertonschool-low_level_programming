55905044337a2b6c8b175d05d2eab2c306161f44
Caaronbll <aaronbll46@gmail.com>
2023-02-12 10:23:33 -0800

    q7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * leet - encodes a string 
+ * @s: string to encode
+ * Return: encoded string
+ */
+
+char *leet(char *)
+{
+	char *r = s;
+	char a[] = { 'a', 'e', 'o', 't', 'l' };
+	char n[] = { 4, 3, 0, 7, 1 };
+	int i = 0;
+
+	while (*s)
+	{
+		for (i = 0;  i < 5; i++)
+		{
+			if (*s == a[i] || *s == a[i] - 32)
+				*s = n[i] + '0';
+		}
+		s++;
+	}
+	return (r);
+}
