cc85e8f7f3809f2c23e7ec2a141e73e735529568
Caaronbll <aaronbll46@gmail.com>
2023-02-12 15:44:22 -0800

    q1

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -13,5 +13,6 @@ char *string_toupper(char *str);
 char *cap_string(char *str);
 char *leet(char *str);
 char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
 
 #endif
