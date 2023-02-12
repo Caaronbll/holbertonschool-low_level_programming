883b54db222716f14bfeb5e2b6b3c63af114ad7a
Caaronbll <aaronbll46@gmail.com>
2023-02-11 16:57:48 -0800

    q6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,28 +7,29 @@
  * Return: capital string
  */
 
-char *cap_string(char *)
+char *cap_string(char *str)
 {
 	int index = 0;
 
 	while (str[index])
 	{
-	if (str[index] == ' ' ||
-		str[index] == '\t' ||
-		str[index] == '\n' ||
-		str[index] == ',' ||
-		str[index] == ';' ||
-		str[index] == '.' ||
-		str[index] == '!' ||
-		str[index] == '?' ||
-		str[index] == '"' ||
-		str[index] == '(' ||
-		str[index] == ')' ||
-		str[index] == '{' ||
-		str[index] == '}' ||
+	index++;
+	if (str[index - 1] == ' ' ||
+		str[index - 1] == '\t' ||
+		str[index - 1] == '\n' ||
+		str[index - 1] == ',' ||
+		str[index - 1] == ';' ||
+		str[index - 1] == '.' ||
+		str[index - 1] == '!' ||
+		str[index - 1] == '?' ||
+		str[index - 1] == '"' ||
+		str[index - 1] == '(' ||
+		str[index - 1] == ')' ||
+		str[index - 1] == '{' ||
+		str[index - 1] == '}' ||
 		index == 0)
 		str[index] -= 32;
-	
+
 	index++;
 	}
 	return (str);
