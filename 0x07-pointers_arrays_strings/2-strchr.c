4b18d5e117454d9b6bb3433ed99198b6ba899973
Caaronbll <aaronbll46@gmail.com>
2023-02-12 16:45:26 -0800

    q2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _strchr - locates a charater in a string
+ * @s: string
+ * @c: charater
+ * Return: pointer
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	if (*s == c)
+		return (s);
+	return (NULL);
+}
