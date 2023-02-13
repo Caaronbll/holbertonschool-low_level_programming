e3316cf395cf9ec38c924dee90d45c2e55025868
Caaronbll <aaronbll46@gmail.com>
2023-02-13 08:15:28 -0800

    q4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * _strpbrk - searches for a set of bytes
+ * @s: string
+ * @accept: bytes to search
+ * Return: pointer to the bytes
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+				return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}
