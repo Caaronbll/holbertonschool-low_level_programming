dcb81cd81fcb9a5fae46811fe97fe3beb050ae30
Caaronbll <aaronbll46@gmail.com>
2023-02-16 14:02:25 -0800

    q5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,27 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * search - search for base
+ * @n: number
+ * @base: base of number
+ * Return: base
+ */
 
+int search(int n, int, base)
+{
+	if (n * n == base)
+		return (n);
+	if (n * n > base)
+		return (-1);
+	return (search(n + 1, base));
+}
+/**
+ * _sqrt_recursion - natural sqr root
+ * @n: number
+ * Return: sqr rt
+ */
+int _sqrt_recursion(int n)
+{
+	return (search(1, n));
+}
