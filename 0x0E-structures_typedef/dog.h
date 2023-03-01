4f967f134b1a0ded7754c11b50ce1c549bac631b
Caaronbll <aaronbll46@gmail.com>
2023-03-01 09:23:36 -0800

    added description for dog structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,12 +3,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * struct dog - structure for a dog containing the name, age and owner
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-	/*structure for a dog */
+
 };
 
 #endif
