1209e0c3ece6eb385568b7a6dad806628b6a9977
Caaronbll <aaronbll46@gmail.com>
2023-03-01 10:27:49 -0800

    q1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -4,7 +4,7 @@
 
 /**
  * init_dog - initializes a variable of type struct dog
- * @dog: structure
+ * @d: structure
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
@@ -15,4 +15,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	d->name = name;
 	d->age = age;
 	(*d).owner = owner;
-}	
+}
