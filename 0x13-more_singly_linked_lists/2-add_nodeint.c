180ffb41535b7d48c0cb1fbce649923b094d0dd4
Caaronbll <aaronbll46@gmail.com>
2023-03-16 15:10:41 -0700

    adding a null terminator

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -10,9 +10,15 @@
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *newnode = malloc(sizeof(listint_t));
-	
+
+	if (*newnode)
+	{
 	newnode->next = *head;
 	newnode->n = n;
 	*head = newnode;
+	}
+	else
+		return (NULL);
+
 	return (*head);
 }
