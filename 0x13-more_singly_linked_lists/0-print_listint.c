07c3aac1326ba39d6369e32d92600805ee7254dd
Caaronbll <aaronbll46@gmail.com>
2023-03-16 07:53:56 -0700

    Quiestion 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,12 +10,9 @@ size_t print_listint(const listint_t *h)
 {
 	const listint_t *temp = h;
 	int nodes = 0;
-	
-	if (!(*h))
-		return (NULL);
 
 	while (temp)
-	{		
+	{
 		printf("%d\n", temp->n);
 		nodes++;
 		temp = temp->next;
