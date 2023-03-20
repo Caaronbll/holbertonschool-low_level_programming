528b39a3e8dd99a257b0bd2b8c8fe1a04357a05c
Caaronbll <aaronbll46@gmail.com>
2023-03-20 12:30:45 -0700

    fixed last three checks

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,11 +8,16 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	int i = 0;
+	int i;
 	unsigned int sum = 0;
 
 	if (!b)
 		return (0);
+
+	for (i = 0;  b[i]; i++)
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+	i = 0;
 
 	while (b[i] == '0' || b[i] == '1')
 	{
