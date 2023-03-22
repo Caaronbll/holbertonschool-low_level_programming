63b8d24b668ed960ef325eb585be9242c1fc684e
Caaronbll <aaronbll46@gmail.com>
2023-03-22 09:18:51 -0700

    adding a nullifier

--- 0x18-doubly_linked_lists/2-add_dnodeint.c
+++ 0x18-doubly_linked_lists/2-add_dnodeint.c
@@ -12,8 +12,13 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *newnode = malloc(sizeof(*newnode));
 	dlistint_t *temp = *head;
 
+	if (!head)
+		return(NULL);
+
 	if (!newnode)
+	{
 		return (NULL);
+	}
 
 	newnode->n = n;
 	newnode->prev = NULL;
