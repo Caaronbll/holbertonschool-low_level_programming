771a2b39426be59950a63eead47cb791988befb2
Caaronbll <aaronbll46@gmail.com>
2023-03-24 09:48:50 -0700

    changed a few things

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,14 +10,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int idx = 0;
 	hash_table_t *new_table = malloc(sizeof(hash_table_t));
 
-	if (!new_table || !size)
+	if (!new_table)
 		return (NULL);
 
 	new_table->array = malloc(sizeof(hash_node_t) * size);
 
 	if (!new_table->array)
 	{
-		free(new_table);
 		return (NULL);
 
 	while (idx < size)
