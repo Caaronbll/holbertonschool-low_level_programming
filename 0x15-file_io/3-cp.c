b566998772be2fd959120dd20e57b05cc444dea6
Caaronbll <aaronbll46@gmail.com>
2023-03-24 09:21:13 -0700

    q0

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * main - program that copies the content of a file to another file
+ * @argc: num argument
+ * @argv: string argument
+ * Return: 0
+ */
+
+
--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,7 +10,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int idx = 0;
 	hash_table_t *new_table = malloc(sizeof(hash_table_t));
 
-	if (!new_table)
+	if (!new_table || !size)
 		return (NULL);
 
 	new_table->array = malloc(sizeof(hash_node_t) * size);
