02d57e2b9d75d0cef68f14801ff84045d3d90485
Caaronbll <aaronbll46@gmail.com>
2023-03-27 15:02:56 -0700

    q3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,6 +1,18 @@
 #include "hash_tables.h"
 
 /**
+ * free_node - Free a node.
+ * @node: Node to free.
+ * Return: Void.
+ */
+void free_node(hash_node_t *node)
+{
+	free(node->key);
+	free(node->value);
+	free(node);
+}
+
+/**
  * hash_table_set - set of the hash table
  * @ht: hash table
  * @key: hey for hash table
