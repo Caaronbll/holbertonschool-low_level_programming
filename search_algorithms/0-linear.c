54422732e66cb4c77186d33aef43d14627c87a99
Caaronbll <aaronbll46@gmail.com>
2023-12-07 14:41:10 -0800

    task 0

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,15 +1,18 @@
-#include "search_algorithms.h"
+#include "search_algos.h"
 
-int linear_search(int *array, size_t size, int value) {
-    
-    if (array == NULL) {
-        return (-1);
-    }
-    for (int i = 0; i < size; i++) {
-        printf("Value checked array[%d] = [%d]\n", i, array[i]);
-        if (array[i] == value) {
-            printf("Found %d at index %d\n", array[i], i);
-            return i;
-        }
-    }
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL) {
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++) {
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value) {
+			return i;
+		}
+	}
+	return (-1);
 }
